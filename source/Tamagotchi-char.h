1ea2b1246da6b1e2bbcba161a2a2d086b1e868e0
EdgarHdz-13 <85575704+EdgarHdz-13@users.noreply.github.com>
2022-11-14 12:28:24 -0600

    initialize

---
+++ source/Tamagotchi-char.h
@@ -0,0 +1,13 @@
+/*
+ * Tamagotchi-char.h
+ *
+ *  Created on: 14 nov 2022
+ *      Author: Edgar
+ */
+
+#ifndef TAMAGOTCHI_CHAR_H_
+#define TAMAGOTCHI_CHAR_H_
+
+
+
+#endif /* TAMAGOTCHI_CHAR_H_ */
